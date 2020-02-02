420c55931775e0e949303c1d9409af9a999ad181
kharioki <antolisah@gmail.com>
2020-02-02 19:19:41 +0300

    added jest, node fetch and created basic tests and asynchronous tests

---
+++ script2.test.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+const swapi = require('./script2');
+
+it('calls swapi to get people', () => {
+  expect.assertions(1);
+  return swapi.getPeople(fetch).then(data => {
+      expect(data.count).toEqual(87)
+  })
+})
+
+it('calls swapi to get people with a promise', () => {
+    expect.assertions(2);
+    return swapi.getPeoplePromise(fetch).then(data => {
+        expect(data.count).toEqual(87);
+        expect(data.results.length).toBeGreaterThan(5);
+    })
+})
+
+it('getPeople returns count and results', () => {
+  const mockFecth = jest.fn()
+    .mockReturnValue(Promise.resolve({
+        json: () => Promise.resolve({
+            count: 87,
+            results: [0,1,2,3,4,5]
+        })
+    }))
+
+    expect.assertions(4)
+    return swapi.getPeoplePromise(mockFecth).then(data => {
+        expect(mockFecth.mock.calls.length).toBe(1);
+        expect(mockFecth).toBeCalledWith('https://swapi.co/api/people');
+        expect(data.count).toEqual(87);
+        expect(data.results.length).toBeGreaterThan(5);
+    });
+})
+
